2d261c5d24951e323a9de2914e98ba11abdea7fa
Nacho Brihuega <brihuega.nacho@gmail.com>
2017-09-04 20:54:49 +0200

    Update getip.py

--- modules/getip/getip.py
+++ modules/getip/getip.py
@@ -1,11 +1,34 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
 import socket
+import requests
+
 def GetIP(target):
+	timeout = 8
 	ip=""
+	valid_responses = ['200', '401', '403', '404', '301', '302']
+
 	try:
-		ip = socket.gethostbyname(target)
+
+		if str(requests.get('http://' + target,timeout = timeout).status_code)  not in valid_responses:
+
+			if str(requests.get('https://' + target, timeout= timeout).status_code) in valid_responses:
+
+				ip = socket.getbyhostname(target)
+
+			else:
+
+				domain_ip = '0.0.0.0'
+				print "The target is not up"
+				exit(0)
+
+		else:
+
+			ip = socket.gethostbyname(target)	
+
 	except Exception as e:
 		print e
-		print "It can't obtain the reverse IP"
-		ip = "0.0.0.0"
-		exit (0)
+		pass
+
 	return ip
